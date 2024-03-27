a3e39b2e1c152a2bdba805b94eab628bbd728258
Blparmar007 <parmarbhavesh5000@gmail.com>
2024-03-27 11:03:54 +0530

    upload video in a post and show it index page and show all the post in profile section (image and video)

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -1,22 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "input", "output" ]
+    static targets = [ "input", "video", "output" ]
 
   connect() {}
 
   readURL() {
+
+    console.log("heloooooooooooooooooooooooooooooo")
+    
     var input = this.inputTarget
-    var output = this.outputTarget
+    //   var output = this.outputTarget
+    
+    
+    if ( input.files[0].type == 'video/mp4') {
+      document.getElementsByClassName('imgBg')[0].innerHTML = `<video src='${(window.URL ? URL : webkitURL).createObjectURL(document.getElementById("upload_file").files[0])}'/>`      
+    }
+    else {
+      document.getElementsByClassName('imgBg')[0].innerHTML = `<image src='${(window.URL ? URL : webkitURL).createObjectURL(document.getElementById("upload_file").files[0])}'/>`
+   } 
 
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function () {
-       output.src = reader.result
-     }
-
-     reader.readAsDataURL(input.files[0]);
-   }
+  // if (input.files[0].type == 'image/jpeg') {
+  //   const p = document.getElementsByClassName('imgBg')
+  //   debugger
+    //       var reader = new FileReader();
+      
+//       reader.onload = function () {
+//         this.imageTarget.src = reader.result
+//       } 
+//       // debugger  
+//   //   console.log("helooooooooooooooooooooooo")
+//    reader.readAsDataURL(input.files[0]);
+//  }
  }
 }
